1cc1e6548087d42c6f53e79518ab21fdbbf5318d
Jorge Omar Negrete <jnegrete@gmail.com>
2025-07-18 11:50:29 -0300

    Inicio del proyecto de extrusión IA -C

--- ingreso.py
+++ ingreso.py
@@ -4,13 +4,13 @@ from db import get_connection
 def mostrar_formulario():
     st.subheader("📋 Ingreso de parámetros de extrusión")
 
-    mat1 = st.number_input("MAT 1 (Hz)", value=60.0)
-    vel_motor_a = st.number_input("Velocidad Motor A", value=65.0)
+    mat1 = st.number_input("MAT 1 (%)", value=60.0)
+    vel_motor_a = st.number_input("Velocidad Motor A (Hz)", value=65.0)
     z1 = st.number_input("Temperatura Z-1 (°C)", value=195.0)
     otc_01 = st.number_input("OTC-01 (°C)", value=90.0)
-    aceptada = st.selectbox("¿Corrida Aceptada?", ["SI", "NO"]) == "SI"
+    aceptada = st.selectbox("¿Placa OK?", ["SI", "NO"]) == "SI"
 
-    if st.button("Guardar corrida"):
+    if st.button("Guardar datos"):
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("""
